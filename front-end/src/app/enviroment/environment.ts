37a0da84c6fef01dd36ded7f7df7679ec784573b
Uzair Ali <chuzairaliuzi877@gmail.com>
2025-11-21 01:45:57 +0500

    working on the env file

---
+++ front-end/src/app/enviroment/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  apiUrl: 'http://localhost:3000',
+  PUBLISHABLE_KEY:
+    'pk_test_51PjSkW1YHObaFJ41pkZYxVAPbeE68etBnoiUT8A54R8dzpnR9UOY9zmctOLyyzcC60A6R4Cl2O7UM9tyM01sPLnG007sQo4kzA',
+  firebaseConfig: {
+      apiKey: "AIzaSyANCu99S9lh69VfRdkQovX1_M-KrZFhkPQ",
+      authDomain: "healthy-food-site.firebaseapp.com",
+      projectId: "healthy-food-site",
+      storageBucket: "healthy-food-site.appspot.com",
+      messagingSenderId: "1008271880596",
+      appId: "1:1008271880596:web:d8f821b7ce392efe795f81",
+      measurementId: "G-QBYH3C7CD7"
+  },
+};
