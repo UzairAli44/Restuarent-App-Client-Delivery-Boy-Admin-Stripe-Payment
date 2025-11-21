97df083eaf35aa09e4e21e4db13eb6b0ec275223
Uzair Ali <chuzairaliuzi877@gmail.com>
2025-11-22 01:19:48 +0500

    working on the dashboard component

---
+++ front-end/src/app/admin/components/dashboard/dashboard.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { MainService } from 'src/app/shared/services/main.service';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+  orders:any = [];
+  deliveryStatuses = ['Pending', 'Shipped', 'Delivered', 'Cancelled'];
+  constructor(private mainService : MainService) {}
+  ngOnInit(): void {
+    this.getAllOrders()
+  }
+
+  getAllOrders() {
+    this.mainService.getAllOrders().subscribe((res:any)=>{
+      this.orders = res.map((item: any) => {
+        item.date = item.date.toDate(); // Convert Firestore Timestamp to Date
+        return item;
+      });
+      console.log(this.orders)
+    })
+  }
+  onStatusChange(order: any) {
+    // Handle the status change logic here
+    this.mainService.updateStatus(order.id,order.status)
+    console.log(`Order ID: ${order.id}, New Status: ${order.status}`);
+    // You can also update the order status in your backend/database if necessary
+  }
+  getProductTitlesAndQuantities(products:any) {
+    return products.map((product:any) => {
+        const { productTitle, quantity } = product;
+        return `Product Title: ${productTitle}, Quantity: ${quantity}`;
+    });
+}
+}
