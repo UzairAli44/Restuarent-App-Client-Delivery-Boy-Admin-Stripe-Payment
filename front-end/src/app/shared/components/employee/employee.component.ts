859bf1226e6b2fd1bc39e0f87deeb76e034e7e5f
Uzair Ali <chuzairaliuzi877@gmail.com>
2025-11-22 20:45:38 +0500

    working on the employee component

---
+++ front-end/src/app/shared/components/employee/employee.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { MainService } from '../../services/main.service';
+
+@Component({
+  selector: 'app-employee',
+  templateUrl: './employee.component.html',
+  styleUrls: ['./employee.component.css']
+})
+export class EmployeeComponent {
+  orders:any = [];
+  deliveryStatuses = ['Pending', 'Shipped', 'Delivered', 'Cancelled'];
+  constructor(private mainService : MainService) {}
+  ngOnInit(): void {
+    this.getAllOrders()
+  }
+
+  getAllOrders() {
+    this.mainService.getAllOrders().subscribe((res:any)=>{
+      this.orders = res.map((item: any) => {
+        item.date = item.date.toDate(); // Convert Firestore Timestamp to Date
+        return item;
+      });
+    })
+  }
+  onStatusChange(order: any) {
+    // Handle the status change logic here
+    this.mainService.updateStatus(order.id,order.status)
+  }
+
+  getProductTitlesAndQuantities(products:any) {
+    return products.map((product:any) => {
+        const { productTitle, quantity } = product;
+        return `Product Title: ${productTitle}, Quantity: ${quantity}`;
+    });
+}
+
+getAddress() {
+  
+}
+}
